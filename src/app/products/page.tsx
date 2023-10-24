6ffa2ce8bc826225998b0cb2032353d7b4e2afee
AndreSuaza <suaza.andre@gmail.com>
2023-10-24 02:13:49 -0500

    cambio temporal en productos

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,55 +2,55 @@ import { ProductView } from "@/components/ProductView/ProductView"
 import { findProducts } from "./Services/Products.services";
 
 async function fetchProducts() {
-  return await findProducts();
+  //return await findProducts();
 }
 
-const featuredProducts = [
-  { id: 1,
-    name: "Mazo Estructurado",
-    code: "1234",
-    price: "",
-    discount: "",
-    releaseDate: "",
-    logo:  "",
-    description:  "",
-    deleteAt:  "",
-    images: ["md-1.jpg"]
-  },
-  { id: 2,
-    name: "Mazo Estructurado",
-    code: "1234",
-    price: "",
-    discount: "",
-    releaseDate: "",
-    logo:  "",
-    description:  "",
-    deleteAt:  "",
-    images: ["md-2.jpg"]
-  },
-  { id: 3,
-    name: "Mazo Estructurado",
-    code: "1234",
-    price: "",
-    discount: "",
-    releaseDate: "",
-    logo:  "",
-    description:  "",
-    deleteAt:  "",
-    images: ["md-3.jpg"]
-  },
-  { id: 4,
-    name: "Mazo Estructurado",
-    code: "1234",
-    price: "",
-    discount: "",
-    releaseDate: "",
-    logo:  "",
-    description:  "",
-    deleteAt:  "",
-    images: ["md-4.jpg"]
-  }
-];
+// const featuredProducts = [
+//   { id: 1,
+//     name: "Mazo Estructurado",
+//     code: "1234",
+//     price: "",
+//     discount: "",
+//     releaseDate: "",
+//     logo:  "",
+//     description:  "",
+//     deleteAt:  "",
+//     images: ["md-1.jpg"]
+//   },
+//   { id: 2,
+//     name: "Mazo Estructurado",
+//     code: "1234",
+//     price: "",
+//     discount: "",
+//     releaseDate: "",
+//     logo:  "",
+//     description:  "",
+//     deleteAt:  "",
+//     images: ["md-2.jpg"]
+//   },
+//   { id: 3,
+//     name: "Mazo Estructurado",
+//     code: "1234",
+//     price: "",
+//     discount: "",
+//     releaseDate: "",
+//     logo:  "",
+//     description:  "",
+//     deleteAt:  "",
+//     images: ["md-3.jpg"]
+//   },
+//   { id: 4,
+//     name: "Mazo Estructurado",
+//     code: "1234",
+//     price: "",
+//     discount: "",
+//     releaseDate: "",
+//     logo:  "",
+//     description:  "",
+//     deleteAt:  "",
+//     images: ["md-4.jpg"]
+//   }
+// ];
 
 
 const Products = async () => {
@@ -59,13 +59,13 @@ const Products = async () => {
 
   return (
     <>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 px-24 justify-items-center mt-20">
+      {/* <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 px-24 justify-items-center mt-20">
         { featuredProducts.map((product) => <ProductView data={product}/>)}
       </div>
       <h2 className="text-6xl uppercase text-center mt-20">Productos</h2>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 px-24 justify-items-center my-20">
         {products.map((product) => <ProductView data={product} detail={true}/>)}
-      </div>
+      </div> */}
       
       
     </>
