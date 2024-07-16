d6cd01ddcce3b4dbe93021a424d85107d73d0637
AsuazaDyN <asuaza@dichter-neira.com>
2024-07-16 11:26:41 -0500

    star competitive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,7 @@ export default function Home() {
           {/* <span>म्रियमाणाः आत्मानः, क्रीडितुं विनोदं च कर्तुं संग्रहणीयः ताशक्रीडा</span> */}
           <h2 className="uppercase mb-2 md:text-4xl stroke lg:text-8xl">Souls Master Series</h2>
           <p className="uppercase text-2xl stroke lg:-4xl">Inicia el competitivo, lucha y gana</p>
-          <p className="uppercase text-4xl stroke lg:text-6xl stroke text-primary">Marzo 1</p>
+          <p className="uppercase text-4xl stroke lg:text-6xl stroke text-primary">NOVIEMBRE 2024</p>
         </div>
       </section>
 
