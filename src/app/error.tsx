1f17be4e08df7b7c6146f4f185150a72ce362a82
AsuazaDyN <asuaza@dichter-neira.com>
2024-09-12 15:16:29 -0500

    fix error 500 in page to 404

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,22 @@
-import Link from "next/link";
-
-export default function NotFound() {
-    return (
-      <div className="min-h-screen bg-[url('/404.jpg')] bg-contain bg-no-repeat bg-center pt-40">
+'use client' // Error boundaries must be Client Components
+ 
+import Link from 'next/link'
+import { useEffect } from 'react'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div className="min-h-screen bg-[url('/404.jpg')] bg-contain bg-no-repeat bg-center pt-40">
         <div className="m-auto text-center">
             <h1 className="text-8xl mb-10">404: ¡Alma Perdida!</h1>
             <h3 className="text-4xl mb-2">Parece que el alma que buscas se ha extinguido...</h3>
@@ -11,6 +25,6 @@ export default function NotFound() {
 
             <Link href="/"><button className="btn mb-10">Regresa a Casa</button></Link>
         </div>
-      </div>
-    );
-  }
+    </div>
+  )
+}
