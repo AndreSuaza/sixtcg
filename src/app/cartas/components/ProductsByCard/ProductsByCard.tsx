721cf35e40a03fabd00e79ed01c440515bccf9a0
AsuazaDyN <asuaza@dichter-neira.com>
2024-09-12 14:54:23 -0500

    fix link products

--- src/app/cartas/components/ProductsByCard/ProductsByCard.tsx
+++ src/app/cartas/components/ProductsByCard/ProductsByCard.tsx
@@ -26,9 +26,10 @@ export const ProductsByCard = ({productsByCard}: Props) => {
 
   return (
     <div className="grid grid-cols-2 gap-2 ml-2">
-         {products && products.map((product) =>
+         {products && products.map((product) => 
+            <>
+            {product.show === "si"?
             <Link key={product.code} href={`/productos/${product.url}`} target="_blank">
-
                 <Image 
                     width={300} 
                     height={300} 
@@ -39,8 +40,25 @@ export const ProductsByCard = ({productsByCard}: Props) => {
                     placeholder="blur"
                 />
                 <h3>{product.name}</h3>
-
             </Link>
+            
+            : 
+            <div>
+                <Image 
+                    width={300} 
+                    height={300} 
+                    src={ product.images?.length > 0 ? `/products/${product.images[0].url}` : `/estructur-1.jpg`} 
+                    alt={product.name} 
+                    className="rounded-t m-auto"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNMTvt4EgAFcwKFsn71ygAAAABJRU5ErkJggg=="
+                    placeholder="blur"
+                />
+                <h3>{product.name}</h3>
+            </div>
+            }
+            
+            </>
+            
          )}
     </div>
   )
