415826ec2114b06448065ed9bf105598e8e75f46
AsuazaDyN <asuaza@dichter-neira.com>
2024-09-25 12:11:23 -0500

    rounded cards

--- src/components/CardView/CardView.tsx
+++ src/components/CardView/CardView.tsx
@@ -24,7 +24,7 @@ const CardView = ({ img, alt, title, zoom = false, border = false }: Props) => {
 
   return (
     <>
-    <div className={`relative ${border && 'border-2 border-white rounded-lg'}`}>
+    <div className={`relative ${border && 'border-2 border-white rounded-2xl'}`}>
     <Image 
       width={500} 
       height={718} 
@@ -33,7 +33,7 @@ const CardView = ({ img, alt, title, zoom = false, border = false }: Props) => {
       title={title}
       blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNMTvt4EgAFcwKFsn71ygAAAABJRU5ErkJggg=="
       placeholder="blur" 
-      className="rounded-lg drop-shadow-md m-auto sm:w-screen p-2"
+      className="rounded-2xl drop-shadow-md m-auto sm:w-screen p-2"
     />
     { zoom && 
       <div className="absolute bottom-0 right-0 mr-1 mt-1 bg-primary p-1 cursor-pointer opacity-100 hover:scale-125 ease-out duration-300" onClick={handlerCardDetail}>
@@ -45,7 +45,7 @@ const CardView = ({ img, alt, title, zoom = false, border = false }: Props) => {
 
     {cardDetail && <div className="fixed z-20 left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10">
       <div className="bg-white p-2 relative">
-        <Image width={380} height={400} src={img} alt={alt} className="border border-zinc-500" placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNcyvC/HgAFGwIlHWlhOQAAAABJRU5ErkJggg=="/>
+        <Image width={380} height={400} src={img} alt={alt} className="border rounded-2xl border-zinc-500" placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNcyvC/HgAFGwIlHWlhOQAAAABJRU5ErkJggg=="/>
         <div className="absolute -top-1 -right-1 mr-1 mt-1 bg-primary hover:bg-indigo-300 p-2 cursor-pointer ease-out duration-300" onClick={handlerCardDetail}>
             <XMarkIcon className="h-6 w-6 text-white"/>
         </div>
