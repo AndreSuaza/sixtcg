23600862775365bebd2302fded6453dc761b1d17
AsuazaDyN <asuaza@dichter-neira.com>
2024-09-29 22:11:21 -0500

    add videos to how to play

--- src/app/como-jugar/page.tsx
+++ src/app/como-jugar/page.tsx
@@ -31,13 +31,26 @@ async function HowToPlay () {
   return (
     <div className="bg-slate-50 text-gray-700 pb-10">
       <section className='bg-[url("/howtoplay/bakeneko.jpg")] w-full h-screen bg-black bg-no-repeat bg-cover relative md:bg-contain lg:bg-left bg-center text-white'>
-          <div className="absolute w-full text-center right-0 lg:w-1/2 lg:mx-20 lg:top-1/3 md:top-1/3 top-2/3">
+          <div className="absolute w-full text-center xl:right-0 lg:right-40 md:right-40 sm:right-0 lg:w-1/2 lg:mx-20 lg:top-1/3 md:top-1/3 top-2/3">
             <p className="uppercase stroke lg:text-6xl md:text-6xl text-2xl mb-10">Te dare un recorrido por Souls of Xtincion</p>
             <h1 className="mx-20 text-4xl stroke">Aprende Cómo jugar y reglas básicas.</h1>
           </div>
       </section>
 
       <div className="how-to-play md:mx-24 mx-4 text-2xl">
+
+        <section className="grid xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 text-center">
+          <div className="mx-auto">
+            <h2 className="my-6">Aprende con Wizard</h2>
+          <iframe className="mx-auto md:w-[560px] md:h-[315px] sm:w-[280px] sm:h-[157px]" src="https://www.youtube.com/embed/NE6FF2iIkHU?si=grYNYcg2zciwoZuW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+          <h2 className="my-6">Aprende con HerosBran</h2>
+          <iframe className="mx-auto md:w-[560px] md:h-[315px] sm:w-[280px] sm:h-[157px]" src="https://www.youtube.com/embed/hbVOkDLkFvM?si=iL869mpe89rplJiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </section>
+
+
         <article className="mt-16">
           <h2 className="mb-6 text-gray-900 text-center">Para jugar Souls in Xtinction necesitas:</h2>          
           <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6">
