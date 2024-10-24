f3bb14a465d9156765d7f218df93302a78ab2d64
AsuazaDyN <asuaza@dichter-neira.com>
2024-10-24 13:08:47 -0500

    cambio titulo producto leyendas peludas

--- src/app/productos/leyendas-peludas/page.tsx
+++ src/app/productos/leyendas-peludas/page.tsx
@@ -216,7 +216,7 @@ const AngelsDeck = async () => {
                 </div>
                     
                 <section className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-1 gap-5 my-16">
-                    <h2 className="text-4xl text-gray-900 lg:col-span-6 md:col-span-4 sm:col-span-1 text-center">Todas las cartas que viene en leyendas peludas:</h2>
+                    <h2 className="text-4xl text-gray-900 lg:col-span-6 md:col-span-4 sm:col-span-1 text-center">Todas las cartas de leyendas peludas:</h2>
                     <Link className="lg:col-span-6 md:col-span-4 sm:col-span-1 m-auto" href={'/cartas?&products=LP'}>
                         <button className="btn hover-primary">Ver detalle de la lista de cartas</button>
                     </Link>
